d1de69f8416c43534d7016b9474230772273b24d
Ahmed Amin <qa.ahmed.amin10@gmail.com>
2020-12-16 18:04:32 +0300

    Add commented steps for the game

--- bin/main.rb
+++ bin/main.rb
@@ -35,8 +35,21 @@ class StartGame
     end
 end
 
-class 
+ 
 
 new_game = StartGame.new(player_one , player_two)
 new_game.greeting
 new_game.print_board([1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9])
+
+puts "#{player_one} turn: Select number between 1 To 9"
+player_one_turn = gets.chomp.to_i
+# method: check the number
+# Change the board if check pass “Now, the move is displayed on the board”
+# Or: Write warning message if invalid number or repeated number
+
+puts "#{player_two} turn: Select number between 1 To 9"
+player_two_turn = gets.chomp.to_i
+# method: check the number
+# Change the board if check pass “Now, the move is displayed on the board”
+# Or: Write warning message if invalid number or repeated number
+
