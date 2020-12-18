54d337b941c38ff810ac9f5e59f0d08ab2b2e22a
Ahmed Amin <qa.ahmed.amin10@gmail.com>
2020-12-19 01:12:12 +0300

    Add another class and remove puts ,print and gets from game logic file

--- bin/main.rb
+++ bin/main.rb
@@ -1,10 +1,10 @@
 #!/usr/bin/env ruby
 require_relative '../lib/game_logic'
 
-puts 'Name of First Player : '
+puts 'The First player name : '
 player_one = gets.chomp
 
-puts 'Name of Seconde Player : '
+puts 'The Second player name : '
 player_two = gets.chomp
 
 flag = false
@@ -13,19 +13,61 @@ arr = []
 new_game = StartGame.new(player_one, player_two)
 new_game.greeting
 new_game.print_board(game_board)
+win_draw_case = WinOrDraw.new
+
+def check(number, player, game_board, arr)
+  if (1..9).include?(number)
+    if arr.include?(number) == true
+      puts 'This number has been taken before! , please try another number'
+      number = gets.chomp.to_i
+      check(number, player, game_board, arr)
+    else
+      arr.push(number)
+      game_board[number - 1] = if player == 1
+                                 'X'
+                               else
+                                 'O'
+                               end
+    end
+  else
+    puts 'Invalid number'
+    puts 'Please select number between 1 and 9'
+    number = gets.chomp.to_i
+    check(number, player, game_board, arr)
+  end
+end
+
+def win_draw (player,flag,draw)
+  if flag == true
+    if draw == false
+       puts "Congratulation #{player} ! You Win " 
+    else
+       puts 'DRAW !'
+    end
+  else
+  puts "Continue in the game !"
+   end
+end
+
+
 
 while flag == false
 
-  puts "#{player_one} turn: Select number between 1 To 9"
+  puts "#{player_one} turn: Select number between 1 and 9"
   player_one_turn = gets.chomp.to_i
-  new_game.check(player_one_turn, 1, game_board, arr)
-  new_game.game_status(game_board, player_one, flag)
-  break if new_game.flag == true
+  check(player_one_turn, 1, game_board, arr)
+  puts new_game.print_board(game_board)
+  win_draw_case.game_status(game_board, flag)
+  win_draw(player_one,win_draw_case.flag ,win_draw_case.draw )
+  break if win_draw_case.flag == true
 
-  puts "#{player_two} turn: Select number between 1 To 9"
+  puts "#{player_two} turn: Select number between 1 and 9"
   player_two_turn = gets.chomp.to_i
-  new_game.check(player_two_turn, 2, game_board, arr)
-  new_game.game_status(game_board, player_two, flag)
-  break if new_game.flag == true
+  check(player_two_turn, 2, game_board, arr)
+  puts new_game.print_board(game_board)
+  win_draw_case.game_status(game_board, flag )
+  win_draw(player_two,win_draw_case.flag ,win_draw_case.draw)
+  break if win_draw_case.flag == true
+
 
 end
--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -1,14 +1,13 @@
 # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Layout/LineLength
 
 class StartGame
-  attr_accessor :flag
-
+ 
   def initialize(player_one, player_two)
     @player_one = player_one
     @player_two = player_two
     @sign_x = 'X'
     @sign_o = 'O'
-    @flag = false
+   
   end
 
   def greeting
@@ -29,50 +28,34 @@ class StartGame
         #{board[6]} | #{board[7]} | #{board[8]}"
   end
 
-  def check(number, player, game_board, arr)
-    if (1..9).include?(number)
-      if arr.include?(number) == true
-        puts 'This number has been taken before! , please try another number'
-        number = gets.chomp.to_i
-        check(number, player, game_board, arr)
-      else
-        arr.push(number)
-        game_board[number - 1] = if player == 1
-                                   'X'
-                                 else
-                                   'O'
-                                 end
-        print_board(game_board)
-      end
-    else
-      puts 'Invalid number'
-      puts 'Please select number between 1 and 9'
-      number = gets.chomp.to_i
-      check(number, player, game_board, arr)
-    end
+end
+
+class WinOrDraw
+  attr_accessor :flag , :draw
+
+  def initialize
+    @flag = false
+    @draw = false
   end
 
-  def game_status(board, player, flag)
+  def game_status(board, flag , draw=false)
     @flag = flag
+    @draw = draw
     if ((board[0] == board[1]) && (board[1] == board[2])) || ((board[3] == board[4]) && (board[4] == board[5])) || ((board[6] == board[7]) && (board[7] == board[8]))
-      puts "Congratulation #{player} ! You Win "
+       
       @flag = true
 
     elsif ((board[0] == board[3]) && (board[3] == board[6])) || ((board[1] == board[4]) && (board[4] == board[7])) || ((board[2] == board[5]) && (board[5] == board[8]))
-      puts "Congratulation #{player} ! You Win "
+       
       @flag = true
 
     elsif ((board[0] == board[4]) && (board[4] == board[8])) || ((board[2] == board[4]) && (board[4] == board[6]))
-      puts "Congratulation #{player} ! You Win "
+      
       @flag = true
 
     elsif board.count('X') > 4
-      puts 'Draw!'
+      @draw = true
       @flag = true
-
-    else
-      puts 'Continue in the game'
-
     end
   end
 end
